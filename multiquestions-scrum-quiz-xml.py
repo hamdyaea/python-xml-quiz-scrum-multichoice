7f3e1f9d72c2fa3b1d65b9fff9b1816628fda27b
Hamdy Abou El Anein <hamdyaea@gmail.com>
2017-11-16 15:56:52 +0100

    commit 2

--- multiquestions-scrum-quiz-xml.py
+++ multiquestions-scrum-quiz-xml.py
@@ -6,13 +6,11 @@ from easygui import *
 
 #please use at least version 0.98  with #sudo python3 -m pip install --upgrade easygui
 
-#import time
 
 import pygame
 
 pygame.init()
 
-#pygame.mixer.init()
 
 import xml.etree.ElementTree as ET
 tree = ET.parse('questions.xml')
@@ -37,46 +35,40 @@ if game_start != "No":
     msgbox(title="Let the Scrum Master Quizz begin",image=logo,msg="Your score is "+str(score))
 
     for question in root:
+        ans = []
+        anstrue =[]
         msg = question.attrib.get("msg")
         type = question.attrib.get("type")
         for answer in question:
-            ans = answer.attrib.get("text")
-            valid = answer.attrib.get("valid") == "true"
+            ans.append(answer.attrib.get("text"))
+            if answer.attrib.get("valid") == "true":
+                anstrue.append(answer.attrib.get("text"))
         if type == "unique":
-            userAnswer = choicebox(msg,ans)
-            if ans == "true":
+            userAnswer = choicebox(msg,"question",ans)
+            if userAnswer in anstrue:
                 score = score + 1
                 correct = ("Well done you got it right. Your score is " + str(score))
                 image = "./images/tick.gif"
                 msgbox(title="CORRECT", image=image, msg=correct)
-                break
+
             else:
 
                 wrong = "I'm sorry that's the wrong answer"
                 image = "./images/cross.gif"
                 msgbox(title="Wrong Answer", image=image, msg=wrong)
-            break
+
         else:
-            userAnswer = multichoicebox(msg,ans)
-        if ans == "true":
+            userAnswer = multchoicebox(msg,"question",ans)
+        if userAnswer in anstrue:
             score = score + 1
             correct = ("Well done you got it right. Your score is " + str(score))
             image = "./images/tick.gif"
             msgbox(title="CORRECT", image=image, msg=correct)
-            break
-        else:
 
+        else:
             wrong = "I'm sorry that's the wrong answer"
             image = "./images/cross.gif"
             msgbox(title="Wrong Answer", image=image, msg=wrong)
-        break
-
-    #Question 1
-
-
-        #msg = "An organization has decided to adopt Scrum, but management wants to change the terminology to fit with terminology already used. What will likely happen if this is done?"
-
-        #qchoices = ans
 
 gameover_good = "./images/logo.gif"
 gameover_bad = "./images/logo.gif"
@@ -84,7 +76,7 @@ gameover_bad = "./images/logo.gif"
 game_over_title = "Scrum Master Quiz"
 msg_bad = ("You have not passed the exam, your score is (under 85%) : "+str(score))
 msg_good = ("You have passed the exam, your score is : "+str(score))
-if score < 3:
+if score < 3: #85% of 4
     game_over = msgbox(title=game_over_title,image=gameover_bad,msg= msg_bad)
 else:
     game_over = msgbox(title=game_over_title,image=gameover_good,msg= msg_good)
